7b6bfb5daf144a6a8fb52b73d3145c77c0485b58
Grace Hsieh <c77507@gmail.com>
2016-02-20 20:28:36 -0500

    Added files via upload
    upload files

---
+++ sushindex/app/assets/javascripts/application.js
@@ -0,0 +1,21 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= main
+//= require jquery-1.11.2.min
+//= ScrollMagic.min
+//= require modernizr-2.8.3-respond-1.4.2.min
+//= debug.addIndicators.min
+//= require_tree .
